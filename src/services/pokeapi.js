5839252fc939b037fe53c4e48bc37691e43a38fd
Wall Monfortt <jose.gual.monfortte.f@gmail.com>
2025-05-08 13:01:24 -0600

    Update pokeapi.js
    feat: add simulated error and loading delay for page 3 in Pokemon list fetch

--- src/services/pokeapi.js
+++ src/services/pokeapi.js
@@ -4,6 +4,12 @@ const BASE_URL = 'https://pokeapi.co/api/v2';
 
 export async function fetchPokemosList(page = 1, limit = 20) {
 
+    if (page === 3) {
+        throw new Error('Error simulado solo en la página 3');
+    }
+
+    await new Promise(res => setTimeout(res, 2000));
+
     const offset = (page - 1) * limit;
     const url = `${BASE_URL}/pokemon?limit=${limit}&offset=${offset}`;
     const response = await axios.get(url);
