898071f0eb986e09b45ba0ec961226bd4e4f7f4d
Wall Monfortt <jose.gual.monfortte.f@gmail.com>
2025-05-09 11:49:18 -0600

    Update PokemonCardList.jsx
    feat: add fallback sprite when dream world image fails to load

--- src/components/PokemonCardList.jsx
+++ src/components/PokemonCardList.jsx
@@ -3,10 +3,13 @@ import { Link } from 'react-router-dom';
 
 function PokemonCardList({ pokemons }) {
   return (
-    <Grid container spacing={3} sx={{ maxWidth: 1040, margin: '0 auto' }} justifyContent="center"> 
+    <Grid container spacing={3} sx={{ maxWidth: 1040, margin: '0 auto' }} justifyContent="center">
       {pokemons.map((pokemon) => {
         const number = pokemon.url.split('/').filter(Boolean).pop();
-        const spriteUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${number}.svg`
+        const dreamWorldUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${number}.svg`;
+        const defaultSpriteUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${number}.png`;
+
+        console.log(pokemon)
         return (
           <Grid item xs={12} sm={6} md={3} key={pokemon.name}>
             <Card
@@ -31,12 +34,16 @@ function PokemonCardList({ pokemons }) {
               }}
             >
               <img
-                src={spriteUrl}
+                src={dreamWorldUrl}
                 alt={pokemon.name}
                 width={180}
                 height={180}
                 style={{ marginBottom: 12, objectFit: 'contain' }}
                 loading='lazy'
+                onError={(e) => {
+                  e.target.onerror = null;
+                  e.target.src = defaultSpriteUrl;
+                }}
               />
               <Typography variant="subtitle2" color="text.secondary" sx={{ mb: 1 }}>{`#${number}`}</Typography>
               <Typography variant="h6" align="center" sx={{ fontWeight: 'bold', textTransform: 'capitalize', fontSize: 16 }}>
